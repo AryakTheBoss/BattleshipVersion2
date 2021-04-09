e7a669785b650c647bcf90693d4c6ceea563765f
Aryak <pandeary12@gmail.com>
2021-04-08 20:05:44 -0400

    board frame made

---
+++ src/com/javamaster44/GameController.java
@@ -0,0 +1,12 @@
+package com.javamaster44;
+
+import javafx.fxml.FXML;
+
+public class GameController {
+
+    @FXML
+    public void initialize(){
+
+    }
+
+}
--- src/com/javamaster44/MainMenu.java
+++ src/com/javamaster44/MainMenu.java
@@ -1,6 +1,12 @@
 package com.javamaster44;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainMenu {
 
@@ -8,6 +14,35 @@ public class MainMenu {
     @FXML
     public void initialize(){
 
+    }
+
+    @FXML
+    public void play(){
+        Parent root = null;
+        Stage primaryStage = new Stage();
+        try { //open main menu
+            root = FXMLLoader.load(getClass().getResource("Game.fxml"));
+            primaryStage.setTitle("Battleship 2.0");
+            primaryStage.setScene(new Scene(root, 590, 475));
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void settings(){
+
+        Parent root = null;
+        Stage primaryStage = new Stage();
+        try { //open main menu
+            root = FXMLLoader.load(getClass().getResource("Settings.fxml"));
+            primaryStage.setTitle("Battleship 2.0");
+            primaryStage.setScene(new Scene(root, 590, 475));
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
